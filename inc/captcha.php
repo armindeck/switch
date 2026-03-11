27abb31f77c7d0ab34d75c4b037c2677b06c20cc
armindeck <dbhsoficial2003@gmail.com>
2026-03-11 15:57:57 -0500

    hCaptcha

---
+++ inc/captcha.php
@@ -0,0 +1,22 @@
+<?php
+class Captcha{
+    protected $secretKey = ''; // Agregar la clave secreta aquí
+    protected $captchaVerificationEndpoint = 'https://hcaptcha.com/siteverify';
+
+    public function checkCaptcha($response){
+        $responseData = json_decode($this->verifyCaptcha([
+            'secret' => $this->secretKey,
+            'response' => $response
+        ]));
+        return $responseData->success;
+    }
+
+    protected function verifyCaptcha($data){
+        $verify = curl_init();
+        curl_setopt($verify, CURLOPT_URL, $this->captchaVerificationEndpoint);
+        curl_setopt($verify, CURLOPT_POST, true);
+        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+        return curl_exec($verify);
+    }
+}
